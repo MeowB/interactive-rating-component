01648a099e88400d857702ded6e43c42e6ae63f5
MeowB <60074375+MeowB@users.noreply.github.com>
2023-12-09 08:29:48 +0100

    finishing the project

--- thanksyou.js
+++ thanksyou.js
@@ -1,6 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
 const value = urlParams.get("value")
 
-if (value){
-	console.log(`received this value: ${value}`)
-}
+let selectedScore = document.querySelector(".selected")
+selectedScore.innerHTML = `You selected ${value} out of 5`
